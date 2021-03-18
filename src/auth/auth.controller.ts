9d6fccae213978bf910b5aa83159d7340f140b60
AdoreJE <thkim4693@naver.com>
2021-03-18 19:30:37 +0900

    install passport jwt library

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from "@nestjs/passport";
+import { LocalAuthGuard } from "./local-auth.guard";
 
 @Controller('auth')
 export class AuthController {
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Req() req) {
     return req.user;
